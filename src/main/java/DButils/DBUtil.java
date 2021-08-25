dc69596fa019505de17f79ffd99d11cabc87ec93
Maria M <muratowa49@gmail.com>
2021-08-25 12:42:18 +0300

    Updated

--- src/main/java/DButils/DBUtil.java
+++ src/main/java/DButils/DBUtil.java
@@ -4,89 +4,29 @@ import java.sql.*;
 
 public class DBUtil {
 
-    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/crud_database";
-    static final String USER = "maria";
-    static final String PASSWORD = "1234";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/crud_database";
+    private static final String USER = "maria";
+    private static final String PASSWORD = "123";
+    private static Connection connection;
 
     public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(DATABASE_URL,USER, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Unable to load class.");
-            e.printStackTrace();
+        if (connection == null) {
+            try {
+                Driver driver = new com.mysql.cj.jdbc.Driver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return connection;
+            return connection;
     }
 
-    /*public static void closeConnection(Connection connection) {
+    public static void closeConnection() {
         try {
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-    }*/
-
-    public static void createTable(String tableName) {
-        Connection connection = getConnection();
-        Statement statement = null;
-        String sql;
-        if(tableName.equals("developer")) {
-            try {
-                statement = connection.createStatement();
-                sql = "create table if not exists `developer` (\n" +
-                        "  `idDeveloper` int NOT NULL AUTO_INCREMENT,\n" +
-                        "  `developerFirstName` varchar(45) NOT NULL,\n" +
-                        "  `developerLastName` varchar(45) NOT NULL,\n" +
-                        "  `idTeam` int DEFAULT NULL,\n" +
-                        "   primary key (`idDeveloper`)\n" +
-                        ") ";
-                statement.execute(sql);
-            }catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if(tableName.equals("team")) {
-            try {
-                statement = connection.createStatement();
-                sql = "create table if not exists `team` (\n" +
-                        "  `idTeam` int NOT NULL AUTO_INCREMENT,\n" +
-                        "  `teamName` varchar(45) NOT NULL,\n" +
-                        "  `status` varchar(45) NOT NULL,\n"+
-                        "   primary key (`idTeam`)\n" +
-                        ") ";
-                statement.execute(sql);
-            }catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if(tableName.equals("skill")) {
-            try {
-                statement = connection.createStatement();
-                sql = "create table if not exists `skill` (\n" +
-                        "  `idSkill` int NOT NULL AUTO_INCREMENT,\n" +
-                        "  `skillName` varchar(45) NOT NULL,\n" +
-                        "   primary key (`idSkill`)\n" +
-                        ") ";
-                statement.execute(sql);
-            }catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if(tableName.equals("skill_developer")) {
-            try {
-                statement = connection.createStatement();
-                sql = "create table if not exists `skill_developer` (\n" +
-                        "  `idSkill` int NOT NULL,\n" +
-                        "  `idDeveloper` int NOT NULL,\n" +
-                        "   primary key (`idSkill`)\n" +
-                        ") ";
-                statement.execute(sql);
-            }catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
--- src/main/java/StartProgram.java
+++ src/main/java/StartProgram.java
@@ -1,8 +1,8 @@
 import DButils.DBUtil;
-import view.ConsoleStarter;
 
 public class StartProgram {
 	public static void main(String[] args) throws Exception {
-		DBUtil.createTable("developer");
+		DBUtil.getConnection();
+		DBUtil.getConnection();
 	}
 }
