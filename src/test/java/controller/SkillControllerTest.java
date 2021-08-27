87ca2ab516b5686c31f90de06d801542859b5778
Maria M <muratowa49@gmail.com>
2021-08-27 15:28:47 +0300

    Updated

--- src/test/java/controller/SkillControllerTest.java
+++ src/test/java/controller/SkillControllerTest.java
@@ -1,21 +1,39 @@
 package controller;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import model.Skill;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 import repository.SkillRepository;
+import repository.jdbcImpl.SkillRepositoryImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 
 public class SkillControllerTest {
 
-    @Mock
-    private SkillRepository skillRepository;
-    @InjectMocks
-    private SkillController underTest;
+    SkillController skillController = Mockito.mock(SkillController.class);
+    SkillRepository skillRepository = Mockito.mock(SkillRepository.class);
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @Test
+    public void testGetAllSkill() {
+        Skill java = new Skill(1L, "java");
+        Skill php = new Skill(2L, "php");
+        List<Skill> skills = new ArrayList<>();
+        skills.add(java);
+        skills.add(php);
+        when(skillRepository.getAll()).thenReturn(skills);
+        Assert.assertEquals(skillController.getAll(), skills);
+    }
+
 
 
 }
